1e7bf912834e8ec77daa9c7101c08d2ce75b85b5
cathnech <catherine.nech@gmail.com>
2020-09-09 04:14:32 +0300

    less4 ИИ почти может) не продумывает ходы, но при возможности либо побеждает либо блокирует

--- src/gb/lesson4/lesFour.java
+++ src/gb/lesson4/lesFour.java
@@ -7,11 +7,44 @@ public class lesFour
 {
     static char[][] map;
     static final int size   =   5;
+	static final int winCount = 4; // кол-во фишек
 
     static final char dot_x = 'X';
     static final char dot_o = 'O';
     static final char dot_empty = '.';
 
+	public static void main(String[] args)
+	{
+		initMap();
+		printMap();
+		while (true) {
+			humanTurn();
+			printMap();
+			if (isWinner2(dot_x))
+			{
+				System.out.println("Победил человек");
+				break;
+			}
+			if (isMapFull())
+			{
+				System.out.println("Ничья");
+				break;
+			}
+			aiTurn();
+			printMap();
+			if (isWinner2(dot_o))
+			{
+				System.out.println("Победил Искуственный Интеллект");
+				break;
+			}
+			if (isMapFull())
+			{
+				System.out.println("Ничья");
+				break;
+			}
+		}
+		System.out.println("Игра закончена");
+	}
     static void initMap()
     {
         map =   new char[size][size];
@@ -55,28 +88,45 @@ public class lesFour
 			System.out.println("Введите координаты в формате X Y");
 			x   =   scanner.nextInt() - 1;
 			y   =   scanner.nextInt() - 1;
-	    } while (!isCellValid(x,y));
+	    } while (isCellValid(x, y));
 	    map[y][x] = dot_x;
     }
 
 	static void aiTurn()
 	{
 		Random random = new Random();
-		int x,y;
+		int x = 0,y = 0,flag = 0;
 
-		do
+		for (int i = 0; i < size; i++)
 		{
-			x   =   random.nextInt(size);
-			y   =   random.nextInt(size);
-		} while (!isCellValid(x,y));
-		System.out.println("Компьютер походил: " + x + " и " + y);
-		map[y][x] = dot_o;
+			for (int j = 0; j < size; j++)
+			{
+				if (checkWinPosition(j,i))
+				{
+					flag = 1;
+					x=j;
+					y=i;
+					break;
+				}
+			}
+			if (flag == 1) break;
+		}
+		if (flag != 1)
+		{
+			do
+			{
+				x   =   random.nextInt(size);
+				y   =   random.nextInt(size);
+			} while (isCellValid(x, y));
+			map[y][x] = dot_o;
+		}
+		System.out.println("Компьютер походил: " + (x+1) + " и " + (y+1));
+
 	}
 
     static boolean isCellValid(int x, int y)
     {
-    	if ( x < 0 || x >= size || y < 0 || y >= size || map[y][x] != dot_empty) return false;
-    	else return true;
+	    return x < 0 || x >= size || y < 0 || y >= size || map[y][x] != dot_empty;
     }
 
 	static boolean isWinner2(char symb)
@@ -84,23 +134,24 @@ public class lesFour
 		int flag1,flag2,flag3,flag4;
 		flag3 = 0;
 		flag4 = 0;
-		int winCount = 3;
 		for (int i = 0; i < size ; i++)
 		{
 			flag1 = 0;
 			flag2 = 0;
 
+			flag3 = (map[i][i] == symb) ? (flag3+1) : 0;
+			flag4 = (map[i][size - 1 - i] == symb) ? (flag4+1) : 0;
+
+			if (flag3 == winCount || flag4 == winCount) return true;
+
 			for (int j = 0; j < size; j++)
 			{
-				flag1 = (map[i][j] == symb) ? flag1+1 : 0;
-				flag2 = (map[j][i] == symb) ? flag2+1 : 0;
+				flag1 = (map[i][j] == symb) ? (flag1+1) : 0;
+				flag2 = (map[j][i] == symb) ? (flag2+1) : 0;
 
 				if (flag1 == winCount || flag2 == winCount) return true;
 			}
 
-			flag3 = (map[i][i] == symb) ? flag3+1 : 0;
-			flag4 = (map[i][size - 1 - i] == symb) ? flag4+1 : 0;
-			if (flag3 == winCount || flag4 == winCount) return true;
 		}
 		return false;
 	}
@@ -117,33 +168,23 @@ public class lesFour
     	return true;
     }
 
-    public static void main(String[] args)
-    {
-	    initMap();
-	    printMap();
-	    while (true) {
-		    humanTurn();
-		    printMap();
-		    if (isWinner2(dot_x)) {
-			    System.out.println("Победил человек");
-			    break;
-		    }
-		    if (isMapFull()) {
-			    System.out.println("Ничья");
-			    break;
-		    }
-		    aiTurn();
-		    printMap();
-		    if (isWinner2(dot_o)) {
-			    System.out.println("Победил Искуственный Интеллект");
-			    break;
-		    }
-		    if (isMapFull()) {
-			    System.out.println("Ничья");
-			    break;
-		    }
-	    }
-	    System.out.println("Игра закончена");
+	static boolean checkWinPosition(int x, int y)
+	{
+		if (map[y][x] != dot_empty) return false;
+		map[y][x] = dot_o;
 
-    }
+		if (isWinner2(dot_o))
+		{
+			return true;
+		}
+		map[y][x] = dot_x;
+		if (isWinner2(dot_x))
+		{
+			map[y][x] = dot_o;
+			return true;
+		}
+		map[y][x] = dot_empty;
+		return false;
+
+	}
 }
